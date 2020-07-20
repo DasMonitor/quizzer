5ab97d6304aedc0e97e9be82cc716987e5f8969e
Bogdan Popescu <68062990+bopopescu@users.noreply.github.com>
2020-07-21 00:15:53 +0300

    Changing the master-slave vocabulary

--- testgui.py
+++ testgui.py
@@ -8,21 +8,21 @@ class MyFirstGUI:
         "...by making this label interactive.",
         "Go on, click on it again.",
     ]
-    def __init__(self, master):
-        self.master = master
-        master.title("A simple GUI")
+    def __init__(self, main):
+        self.main = main
+        main.title("A simple GUI")
 
         self.label_index = 0
         self.label_text = StringVar()
         self.label_text.set(self.LABEL_TEXT[self.label_index])
-        self.label = Label(master, textvariable=self.label_text)
+        self.label = Label(main, textvariable=self.label_text)
         self.label.bind("<Button-1>", self.cycle_label_text)
         self.label.pack()
 
-        self.greet_button = Button(master, text="Greet", command=self.greet)
+        self.greet_button = Button(main, text="Greet", command=self.greet)
         self.greet_button.pack()
 
-        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button = Button(main, text="Close", command=main.quit)
         self.close_button.pack()
 
     def greet(self):
